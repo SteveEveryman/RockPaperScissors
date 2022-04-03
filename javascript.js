4484934e7cdf07d253382259bfcc91197c6b2aa9
Eamon Quinn Cleary <eamoncleary89@gmail.com>
2022-04-03 16:03:31 -0400

    Create gameSimulator function to simulate RPS.
    Created the main function to actually play the game. The function
    takes a random computer input and a player input then compares them
    to see who has won. The function has not yet been tested.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,62 @@
 function computerPlay(){
-    let computerMove = ['Rock', 'Paper', 'Scissors'];
+    let computerMove = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * computerMove.length);
     return computerMove[randomIndex];
 }
+
+function gameSimulator(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection = 'rock')
+    {
+        if(computerSelection = 'rock')
+        {
+            return 'Two rocks is a draw!';
+        }
+        else if (computerSelection = 'paper')
+        {
+            return 'Paper beats rock you failure!';
+        }
+        else
+        {
+            return 'Rock beats scissors, you win!'
+        }
+    }
+    else if (playerSelection = 'paper')
+    {
+        if(computerSelection = 'rock')
+        {
+            return 'Paper beats rock, you kick ass!';
+        }
+        else if (computerSelection = 'paper')
+        {
+            return 'Two papers is a draw!';
+        }
+        else
+        {
+            return 'Paper is sliced in twain by scissors. FATALITY!';
+        }
+    }
+    else if (playerSelection = 'scissors')
+    {
+        if(computerSelection = 'rock')
+        {
+            return 'Your puny scissors have been crushed';
+        }
+        else if (computerSelection = 'paper')
+        {
+            return 'You rip apart their fragile paper';
+        }
+        else
+        {
+            return 'Two scissors bounce harmlessly off each other. It is a draw!';
+        }
+    }
+    else
+    {
+        return 'Something went horribly wrong. Check the code';
+    }
+}
+
+playerSelection = 'rock'
+computerSelection = computerPlay();
